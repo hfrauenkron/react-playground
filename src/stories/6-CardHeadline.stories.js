194a229a534f0359ac613856a9ef90dbe0bf524a
Hanna Frauenkron <h.frauenkron@mail.de>
2019-09-23 10:43:38 +0200

    add CardHeadline

---
+++ src/stories/6-CardHeadline.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CardHeadline from "../components/CardHeadline";
+
+export default {
+  title: "Headline"
+};
+
+export const Sample = () => <CardHeadline children={"Test-Headline"} />;
